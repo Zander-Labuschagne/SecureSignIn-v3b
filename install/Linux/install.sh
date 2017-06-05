49a0a0b8649a1f1d07bcfdf2f2cc9a6db4665ecc
Zander Labuschagne <zander.labuschagne@protonmail.ch>
2017-06-05 21:57:19 +0000

    modified:   install/Linux/install.sh

--- install/Linux/install.sh
+++ install/Linux/install.sh
@@ -2,3 +2,6 @@ sudo rm -r /opt/SecureSignIn/
 sudo mkdir /opt/SecureSignIn
 sudo cp -p SecureSignIn-3.3.jar SecureSignIn-3.3.sh ssi.png /opt/SecureSignIn/
 sudo cp -p SecureSignIn.desktop /usr/share/applications/
+sudo chmod +x /usr/share/applications/SecureSignIn.desktop
+sudo chmod +x /opt/SecureSignIn/SecureSignIn-3.3.jar
+sudo chmod +x /opt/SecureSignIn/SecureSignIn-3.3.sh
