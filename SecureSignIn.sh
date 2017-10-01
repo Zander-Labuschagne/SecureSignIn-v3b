bc0a56d221e01f7fdd1d16ab4cb69382c9bb89c1
Zander Labuschagne <zander.labuschagne@protonmail.ch>
2017-10-01 17:36:24 +0200

    Added  v1.0 branch

--- SecureSignIn.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-cd /Users/ZanderMac/Dropbox/NWU\ Pukke/\~\ Java\ Programming/Console\ Applications/2015/Secure\ Sign\ In\(Prototype\)/bin
-javac -cp '.:activation.jar:javax.mail.jar' SecureSignIn.java
-java -cp '.:activation.jar:javax.mail.jar' SecureSignIn
-sleep 5
-osascript -e 'tell application "Terminal" to quit' &
-exit 0
